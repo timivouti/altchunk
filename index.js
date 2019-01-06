7b0ad7c9f4195a1e9a97347e8fb1bd34d91fea69
Timi Voutilainen <timi_v@hotmail.com>
2019-01-06 03:22:25 +0200

    [Content-Types].xml, document.xml, document.xml.rels
    First working versio

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var Docxtemplater = require('docxtemplater');
 var JSZip = require('jszip');
 
 export const mergeDocuments = (firstDocument, files, isDraft) => {
+  var watermark = `<w:sdt><w:sdtPr><w:id w:val="-1960019101"/><w:docPartObj><w:docPartGallery w:val="Watermarks"/><w:docPartUnique/></w:docPartObj></w:sdtPr><w:sdtContent><w:r><w:pict w14:anchorId="59CEE41C"><v:shapetype id="_x0000_t136" coordsize="21600,21600" o:spt="136" adj="10800" path="m@7,l@8,m@5,21600l@6,21600e"><v:formulas><v:f eqn="sum #0 0 10800"/><v:f eqn="prod #0 2 1"/><v:f eqn="sum 21600 0 @1"/><v:f eqn="sum 0 0 @2"/><v:f eqn="sum 21600 0 @3"/><v:f eqn="if @0 @3 0"/><v:f eqn="if @0 21600 @1"/><v:f eqn="if @0 0 @2"/><v:f eqn="if @0 @4 21600"/><v:f eqn="mid @5 @6"/><v:f eqn="mid @8 @5"/><v:f eqn="mid @7 @8"/><v:f eqn="mid @6 @7"/><v:f eqn="sum @6 0 @5"/></v:formulas><v:path textpathok="t" o:connecttype="custom" o:connectlocs="@9,0;@10,10800;@11,21600;@12,10800" o:connectangles="270,180,90,0"/><v:textpath on="t" fitshape="t"/><v:handles><v:h position="#0,bottomRight" xrange="6629,14971"/></v:handles><o:lock v:ext="edit" text="t" shapetype="t"/></v:shapetype><v:shape id="PowerPlusWaterMarkObject357831064" o:spid="_x0000_s2049" type="#_x0000_t136" style="position:absolute;left:0;text-align:left;margin-left:0;margin-top:0;width:412.4pt;height:247.45pt;rotation:315;z-index:-251656704;mso-position-horizontal:center;mso-position-horizontal-relative:margin;mso-position-vertical:center;mso-position-vertical-relative:margin" o:allowincell="f" fillcolor="silver" stroked="f"><v:fill opacity=".5"/><v:textpath style="font-family:&quot;calibri&quot;;font-size:1pt" string="LUONNOS"/><w10:wrap anchorx="margin" anchory="margin"/></v:shape></w:pict></w:r></w:sdtContent></w:sdt>`;
+
   String.prototype.splice = function (idx, rem, str) {
     return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
   };
@@ -40,7 +42,18 @@ export const mergeDocuments = (firstDocument, files, isDraft) => {
 		return out;
 	}
 
-  var zip = new JSZip(firstDocument);
+  const zip = new JSZip(firstDocument);
+
+  var document = utf8ArrayToString(zip.file("word/document.xml")._data.getContent());
+  var newDocument = document;
+
+  var documentXmlRels = utf8ArrayToString(zip.file("word/_rels/document.xml.rels")._data.getContent());
+  var newDocumentXmlRels = documentXmlRels;
+
+  var contentType = utf8ArrayToString(zip.file("[Content_Types].xml")._data.getContent());
+  var newContentType = contentType.splice(contentType.indexOf("</Types>"), 0, `<Default ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml" Extension="docx"/>`);
+
+  zip.file("[Content_Types].xml", newContentType.replace(` standalone="true"`, ""))
 
   if (isDraft && zip.file("word/header3.xml") != null) {
     var addHeader = utf8ArrayToString(zip.file("word/header3.xml")._data.getContent());
@@ -49,26 +62,18 @@ export const mergeDocuments = (firstDocument, files, isDraft) => {
   }
 
   files.map((x, i) => {
-    var docxZip = new JSZip(x);
-    var docxTemp = new Docxtemplater().loadZip(docxZip);
+    zip.file(`word/afchunk${i + 1}.docx`, x, { binary: true });
+    try {
+      newDocument = newDocument.splice(document.lastIndexOf("<w:sectPr"), 0, `<w:altChunk r:id="AltChunkId${i + 1}"/>`);
 
-    docxTemp.render();
-
-    var docxFile = docxTemp.getZip().generate({
-          type: "blob",
-          mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        });
-        
-    zip.file(`word/afchunk{i+1}.docx`, docxFile);
-
-    var document = utf8ArrayToString(zip.file("word/document.xml")._data.getContent());
-    var newDocument = document.splice(0, document.lastIndexOf("<w:sectPr>"), `<w:altChunk r:id="AltChunkId${i}"/>`);
-    zip.file("word/document.xml", newDocument);
-
-    var documentXmlRels = utf8ArrayToString(zip.file("word/_rels/document.xml.rels")._data.getContent());
-    var newDocumentXMlRels = documentXmlRels(0, documentXmlRels.lastIndexOf("</Relationships>"), `<Relationship Id="AltChunkId${i}" Target="/word/afchunk${i}.docx" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/aFChunk"/>`);
-    zip.file("word/_rels/document.xml.rels", newDocumentXmlRels);
+      newDocumentXmlRels = newDocumentXmlRels.splice(documentXmlRels.indexOf("</Relationships>"), 0, `<Relationship Id="AltChunkId${i + 1}" Target="/word/afchunk${i + 1}.docx" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/aFChunk"/>`);
+    } catch (err) {
+      console.log(err);
+    }
   });
+
+  zip.file("word/document.xml", newDocument);
+  zip.file("word/_rels/document.xml.rels", newDocumentXmlRels);
 
   var doc = new Docxtemplater().loadZip(zip);
 
